992300bfa1d5b220cce98006e60391291b6352ac
Mario Macai <mario.macai@accenture.com>
2017-04-03 21:40:58 +0200

    update from pwa

--- service-worker.js
+++ service-worker.js
@@ -12,20 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
-
-// DO NOT EDIT THIS GENERATED OUTPUT DIRECTLY!
-// This file should be overwritten as part of your build process.
-// If you need to extend the behavior of the generated service worker, the best approach is to write
-// additional code and include it using the importScripts option:
-//   https://github.com/GoogleChrome/sw-precache#importscripts-arraystring
-//
-// Alternatively, it's possible to make changes to the underlying template file and then use that as the
-// new base for generating output, via the templateFilePath option:
-//   https://github.com/GoogleChrome/sw-precache#templatefilepath-string
-//
-// If you go that route, make sure that whenever you update your sw-precache dependency, you reconcile any
-// changes made to this original template file with your modified copy.
+ */
 
 // This generated service worker JavaScript will precache your site's resources.
 // The code needs to be saved in a .js file at the top-level of your site, and registered
@@ -37,7 +24,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","be38be1d5fd793572ce6675cdcd6bca4"],["manifest.json","a30758a8c7b71bbc2ac9a632220ac955"],["static/css/main.9a0fe4f1.css","3473922d6aed4c20bb69846d6027cacf"],["static/js/main.78981f2a.js","15d1d4e7cdd5549ef252669c6198a542"],["static/media/logo.5d5d9eef.svg","5d5d9eefa31e5e13a6610d9fa7a283bb"]];
+var precacheConfig = [["favicon.ico","93ffb20d77327583892ca47f597b77aa"],["index.html","f24caeb556a4e8c605f18b74d3f0ea1e"],["manifest.json","a30758a8c7b71bbc2ac9a632220ac955"],["static/css/main.9a0fe4f1.css","3473922d6aed4c20bb69846d6027cacf"],["static/js/main.e4244ba1.js","29907fa3f1acd770e414920f45df0197"],["static/media/logo.5d5d9eef.svg","5d5d9eefa31e5e13a6610d9fa7a283bb"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
@@ -111,7 +98,7 @@ var urlsToCacheKeys = new Map(
     var relativeUrl = item[0];
     var hash = item[1];
     var absoluteUrl = new URL(relativeUrl, self.location);
-    var cacheKey = createCacheKey(absoluteUrl, hashParamName, hash, /\.\w{8}\./);
+    var cacheKey = createCacheKey(absoluteUrl, hashParamName, hash, false);
     return [absoluteUrl.toString(), cacheKey];
   })
 );
@@ -134,19 +121,14 @@ self.addEventListener('install', function(event) {
           Array.from(urlsToCacheKeys.values()).map(function(cacheKey) {
             // If we don't have a key matching url in the cache already, add it.
             if (!cachedUrls.has(cacheKey)) {
-              return cache.add(new Request(cacheKey, {
-                credentials: 'same-origin',
-                redirect: 'follow'
-              }));
+              return cache.add(new Request(cacheKey, {credentials: 'same-origin'}));
             }
           })
         );
       });
     }).then(function() {
-      
       // Force the SW to transition from installing -> active state
       return self.skipWaiting();
-      
     })
   );
 });
@@ -166,9 +148,7 @@ self.addEventListener('activate', function(event) {
         );
       });
     }).then(function() {
-      
       return self.clients.claim();
-      
     })
   );
 });
@@ -210,12 +190,7 @@ self.addEventListener('fetch', function(event) {
     if (shouldRespond) {
       event.respondWith(
         caches.open(cacheName).then(function(cache) {
-          return cache.match(urlsToCacheKeys.get(url)).then(function(response) {
-            if (response) {
-              return response;
-            }
-            throw Error('The cached response that was expected is missing.');
-          });
+          return cache.match(urlsToCacheKeys.get(url));
         }).catch(function(e) {
           // Fall back to just fetch()ing the request if some unexpected error
           // prevented the cached response from being valid.
