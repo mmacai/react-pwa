508318f8c3ddbba9ae79d814705ded2a2e88c62c
Mario Macai <mario.macai@accenture.com>
2017-04-04 11:37:31 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","e2ce4cb47c247ae44a3d94db10e42f88"],["manifest.json","2fc589a909af77f7e6bebc02ca99b57c"],["static/css/main.9a0fe4f1.css","3473922d6aed4c20bb69846d6027cacf"],["static/js/main.523b643c.js","d3a01957678b355d42f5c6cd08cca023"],["static/media/logo.5d5d9eef.svg","5d5d9eefa31e5e13a6610d9fa7a283bb"]];
+var precacheConfig = [["index.html","e2ce4cb47c247ae44a3d94db10e42f88"],["manifest.json","229099ce9f219f17f4f7f2971e55ddb0"],["static/css/main.9a0fe4f1.css","3473922d6aed4c20bb69846d6027cacf"],["static/js/main.523b643c.js","d3a01957678b355d42f5c6cd08cca023"],["static/media/logo.5d5d9eef.svg","5d5d9eefa31e5e13a6610d9fa7a283bb"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
